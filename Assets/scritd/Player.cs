ae8e07a6bb0c5c4af6834416eb09e7219e5998de
Alejandro Villadiego Tuiran <alejopoter@gmail.com>
2017-10-24 20:20:50 -0500

    Add files via upload

---
+++ Assets/scritd/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Player : Secuencia
+{
+
+
+   
+    public float speed;
+
+ 
+    Vector3 target;
+
+    void Start()
+    {
+        
+        target = transform.position;
+    }
+
+    void Update()
+    {
+
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            target.z = 0f;
+        }
+
+      
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        Debug.DrawLine(transform.position, target, Color.green);
+    }
+}
+
+
---
+++ Assets/scritd/jump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+public class jump : MonoBehaviour {
+
+   // Use this for initialization
+   void Start () {
+
+       {
+           RotateView();
+           // the jump state needs to read here to make sure it is not missed
+           if (!m_Jump)
+           {
+               m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+           }
+
+           if (!m_PreviouslyGrounded && m_CharacterController.isGrounded)
+           {
+               StartCoroutine(m_JumpBob.DoBobCycle());
+               PlayLandingSound();
+               m_MoveDir.y = 0f;
+               m_Jumping = false;
+           }
+           if (!m_CharacterController.isGrounded && !m_Jumping && m_PreviouslyGrounded)
+           {
+               m_MoveDir.y = 0f;
+           }
+
+           m_PreviouslyGrounded = m_CharacterController.isGrounded;
+       }
+
+   }
+}
+*/
+
